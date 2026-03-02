1ff68e3e82bd3cfe5d5f74ec220d28d2479a12de
ZahidRr <zahidrobbani44@gmail.com>
2026-03-03 06:44:01 +0700

    Force view compiled path in index.php

--- api/index.php
+++ api/index.php
@@ -1,7 +1,13 @@
 <?php
 
+// 1. Paksa environment variable dari dalam kode PHP
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/views';
+$_SERVER['VIEW_COMPILED_PATH'] = '/tmp/views';
+
+// 2. Buat folder sementara
 if (!file_exists('/tmp/views')) {
     mkdir('/tmp/views', 0777, true);
 }
 
+// 3. Teruskan ke Laravel
 require __DIR__ . '/../public/index.php';
